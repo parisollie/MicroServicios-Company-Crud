615d4b442c17b89338c6221c441be49a953d9d77
parisollie <pjff18@gmail.com>
2025-01-09 00:32:05 -0600

    Seccion 3 completa

--- companies-crud/src/main/java/com/pjff/companies_crud/CompaniesCrudApplication.java
+++ companies-crud/src/main/java/com/pjff/companies_crud/CompaniesCrudApplication.java
@@ -2,8 +2,11 @@ package com.pjff.companies_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// Vid 89
+@EnableDiscoveryClient
 public class CompaniesCrudApplication {
 
 	public static void main(String[] args) {
